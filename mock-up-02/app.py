cad353ecbca14dbe7ada7a4761a7ef7c7ce8d197
OgawaShuhei <fca22060004@edu.fca.ac.jp>
2024-11-11 19:29:53 +0900

    test

--- mock-up-02/app.py
+++ mock-up-02/app.py
@@ -63,37 +63,37 @@ def get_clothing_advice(temp):
     if temp <= 0.0:
         return {
             "advice": "非常に寒いです。モフモフのコートやダウンジャケット、マフラー、手袋を着用してください。",
-            "image_url": "https://example.com/images/very_cold.png"
+            "image_url": "https://hokkaido-labo.com/wp-content/uploads/2024/02/outfitofwintermv.jpg"
         }
     elif 0.1 <= temp <= 5.0:
         return {
             "advice": "寒いです。ダウンコートやジャケット、マフラー、手袋を着用してください。",
-            "image_url": "https://example.com/images/cold.png"
+            "image_url": "https://img.benesse-cms.jp/thank-you/item/image/normal/cfbbc85f-57dd-4909-8b25-d5583c5a24f1.jpg?w=720&h=540&resize_type=cover&resize_mode=force"
         }
     elif 5.1 <= temp <= 8.0:
         return {
             "advice": "少し寒いです。ジャケットやコート、マフラー、手袋を着用してください。",
-            "image_url": "https://example.com/images/slightly_cold.png"
+            "image_url": "https://img.benesse-cms.jp/thank-you/item/image/normal/03ca3771-7d35-4fd4-961f-11c3a9b0cb12.jpg?w=720&h=540&resize_type=cover&resize_mode=force"
         }
     elif 8.1 <= temp <= 13.0:
         return {
             "advice": "肌寒いです。ジャケットやコートを着用してください。",
-            "image_url": "https://example.com/images/chilly.png"
+            "image_url": "https://www.plst.com/jp/ja/news/topics/2023110201/img/18T_231101Zf0KQM.jpg"
         }
     elif 13.1 <= temp <= 16.0:
         return {
             "advice": "晴れで無風なら軽い羽織り物でOKですが、風がある場合はジャケットやコートを着用してください。",
-            "image_url": "https://example.com/images/mild.png"
+            "image_url": "https://cdn-ua.clipkit.co/tenants/1/articles/images/000/005/578/large/2f442f53-3ea2-45aa-9fa3-11efc45b32f8.png?1724226892&p=t"
         }
     elif 16.1 <= temp <= 19.0:
         return {
             "advice": "晴れで風がなければカーディガンでOKですが、風がある場合はジャケットや薄手のコートを着用してください。",
-            "image_url": "https://example.com/images/comfortable.png"
+            "image_url": "https://image.veryweb.jp/wp-content/uploads/2023/11/00-1.jpg"
         }
     elif 19.1 <= temp <= 22.0:
         return {
             "advice": "快適な気温です。晴れで風がなければ上着なしでOKですが、長袖の服が良いでしょう。",
-            "image_url": "https://example.com/images/pleasant.png"
+            "image_url": "https://precious.ismcdn.jp/mwimgs/4/5/-/img_4562c70d042456ebbc76b8a3f30da43370153.jpg"
         }
     elif 22.1 <= temp <= 24.0:
         return {
