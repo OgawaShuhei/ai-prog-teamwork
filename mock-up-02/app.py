2c30d65a1f70b43f5c02834b28b5ca6b22df596c
OgawaShuhei <fca22060004@edu.fca.ac.jp>
2024-11-11 19:33:52 +0900

    return

--- mock-up-02/app.py
+++ mock-up-02/app.py
@@ -61,50 +61,23 @@ def translate_text():
 
 def get_clothing_advice(temp):
     if temp <= 0.0:
-        return {
-            "advice": "非常に寒いです。モフモフのコートやダウンジャケット、マフラー、手袋を着用してください。",
-            "image_url": "https://hokkaido-labo.com/wp-content/uploads/2024/02/outfitofwintermv.jpg"
-        }
+        return "非常に寒いです。モフモフのコートやダウンジャケット、マフラー、手袋を着用してください。"
     elif 0.1 <= temp <= 5.0:
-        return {
-            "advice": "寒いです。ダウンコートやジャケット、マフラー、手袋を着用してください。",
-            "image_url": "https://img.benesse-cms.jp/thank-you/item/image/normal/cfbbc85f-57dd-4909-8b25-d5583c5a24f1.jpg?w=720&h=540&resize_type=cover&resize_mode=force"
-        }
+        return "寒いです。ダウンコートやジャケット、マフラー、手袋を着用してください。"
     elif 5.1 <= temp <= 8.0:
-        return {
-            "advice": "少し寒いです。ジャケットやコート、マフラー、手袋を着用してください。",
-            "image_url": "https://img.benesse-cms.jp/thank-you/item/image/normal/03ca3771-7d35-4fd4-961f-11c3a9b0cb12.jpg?w=720&h=540&resize_type=cover&resize_mode=force"
-        }
+        return "少し寒いです。ジャケットやコート、マフラー、手袋を着用してください。"
     elif 8.1 <= temp <= 13.0:
-        return {
-            "advice": "肌寒いです。ジャケットやコートを着用してください。",
-            "image_url": "https://www.plst.com/jp/ja/news/topics/2023110201/img/18T_231101Zf0KQM.jpg"
-        }
+        return "肌寒いです。ジャケットやコートを着用してください。"
     elif 13.1 <= temp <= 16.0:
-        return {
-            "advice": "晴れで無風なら軽い羽織り物でOKですが、風がある場合はジャケットやコートを着用してください。",
-            "image_url": "https://cdn-ua.clipkit.co/tenants/1/articles/images/000/005/578/large/2f442f53-3ea2-45aa-9fa3-11efc45b32f8.png?1724226892&p=t"
-        }
+        return "晴れで無風なら軽い羽織り物でOKですが、風がある場合はジャケットやコートを着用してください。"
     elif 16.1 <= temp <= 19.0:
-        return {
-            "advice": "晴れで風がなければカーディガンでOKですが、風がある場合はジャケットや薄手のコートを着用してください。",
-            "image_url": "https://image.veryweb.jp/wp-content/uploads/2023/11/00-1.jpg"
-        }
+        return "晴れで風がなければカーディガンでOKですが、風がある場合はジャケットや薄手のコートを着用してください。"
     elif 19.1 <= temp <= 22.0:
-        return {
-            "advice": "快適な気温です。晴れで風がなければ上着なしでOKですが、長袖の服が良いでしょう。",
-            "image_url": "https://precious.ismcdn.jp/mwimgs/4/5/-/img_4562c70d042456ebbc76b8a3f30da43370153.jpg"
-        }
+        return "快適な気温です。晴れで風がなければ上着なしでOKですが、長袖の服が良いでしょう。"
     elif 22.1 <= temp <= 24.0:
-        return {
-            "advice": "半袖で過ごせますが、薄いカーディガンがあると安心です。",
-            "image_url": "https://example.com/images/warm.png"
-        }
+        return "半袖で過ごせますが、薄いカーディガンがあると安心です。"
     else:
-        return {
-            "advice": "暑いです。半袖で過ごせます。",
-            "image_url": "https://example.com/images/hot.png"
-        }
+        return "暑いです。半袖で過ごせます。"
 
 if __name__ == '__main__':
     app.run(debug=True)
